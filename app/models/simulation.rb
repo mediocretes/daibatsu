193564599a838b1145ec11b1774c936ce365276e
Nathan Acuff <naginata@gmail.com>
2011-02-09 20:09:13 -0500

    finished simulation

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -1,24 +1,24 @@
 class Simulation < ActiveRecord::Base
   belongs_to :punishment
+  @queue = :simulation
+  @unique_jobs = true
+  @delayed_jobs = true
   
   def simulate
+    Resque.enable_delay(:simulation)
+    Resque.enqueue(Simulation, {:_id => submitted_by + punishment.id.to_s, :punishment_id => punishment.id, :delay_until => Time.now + 60, :submitted_by => submitted_by})
+  end
+
+  def actually_simulate
     sleep 5
     self.dislike = rand 100 + 1
     return save
-  end
+  end  
 
-  #def self.perform(options)
-  #  punishment = Punishment.find options['punishment_id']
-  #  simulation = punishment.simulations.new
-  #  simulation.submitted_by = options['submitted_by'] + ' queued'
-  #  simulation.actually_simulate
-  #end    
-  # @queue = :simulation
-  # @delayed_jobs = true
-  # @unique_jobs = true
-  # QUEUE=* rake environment resque:work
-  #Resque.enqueue(Simulation, { :punishment_id => punishment.id, :submitted_by => submitted_by})    
-  #Resque.enable_delay(:simulation)
-  #Resque.enqueue(Simulation, {:_id => submitted_by + punishment.id.to_s, :punishment_id => punishment.id, 
-  #                 :submitted_by => submitted_by, :delay_until => Time.now + 300})    
+  def self.perform(options)
+    punishment = Punishment.find(options['punishment_id'])
+    simulation = punishment.simulations.new
+    simulation.submitted_by = options['submitted_by']
+    simulation.actually_simulate
+  end    
 end
