998f773b95675c499a0399ec753526d478f86a16
Nathan Acuff <naginata@gmail.com>
2011-03-09 17:15:47 -0500

    integrate whenever

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -1,16 +1,7 @@
 class Simulation < ActiveRecord::Base
   belongs_to :punishment
-  @queue = :simulation
-  @unique_jobs = true
-  @delayed_jobs = true
-  
-  def simulate
-    Resque.enable_delay(:simulation)
-    Resque.enqueue(Simulation, {:_id => submitted_by + punishment.id.to_s, :punishment_id => punishment.id, :delay_until => Time.now + 60, :submitted_by => submitted_by})
-  end
 
   def actually_simulate
-    sleep 5
     self.dislike = rand 100 + 1
     return save
   end  
@@ -21,4 +12,12 @@ class Simulation < ActiveRecord::Base
     simulation.submitted_by = options['submitted_by']
     simulation.actually_simulate
   end    
+  
+  def self.cron_simulate
+    Punishment.find(:all).each do |punishment|
+      simulation = punishment.simulations.new
+      simulation.submitted_by = 'cron'
+      simulation.actually_simulate
+    end
+  end
 end
---
+++ config/schedule.rb
@@ -0,0 +1,3 @@
+every 1.day :at => '12:01 am' do
+  runner 'Simulation.cron_simulate'
+end
